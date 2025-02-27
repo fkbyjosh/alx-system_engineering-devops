a9fb452199c14d2801b507480e55545c12386ce6
Joshua Baka <fatow.jay@gmail.com>
2025-02-27 00:45:13 +0000

    corrections to 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)"
@@ -13,10 +13,17 @@ v1.0.0 (by /u/firdaus_cartoon_jr)"
     params = {
         "limit": 10
     }
+    
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    
+    # Check for any non-200 status code
+    if response.status_code != 200:
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    
+    try:
+        results = response.json().get("data")
+        [print(c.get("data").get("title")) for c in results.get("children")]
+    except Exception:
+        print(None)
