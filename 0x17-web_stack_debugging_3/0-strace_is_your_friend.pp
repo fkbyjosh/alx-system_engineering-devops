ae5af180acae70e52d0527f8fa7e0fc037e57036
Joshua Baka <fatow.jay@gmail.com>
2025-03-01 20:27:31 +0000

    restore

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
 # Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin/', '/usr/bin/'],
-  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
-}
-
-# Ensure Apache service is running and is restarted after the WordPress fix
-service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  require    => Exec['fix-wordpress'],
-  restart    => true,
+  path    => '/usr/local/bin/:/bin/'
 }
